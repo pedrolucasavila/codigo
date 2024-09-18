e70c7f72cf990be980ea6ab80fecf37d24feea09
pedrolucasavila <pedrocavalcanti772@gmail.com>
2024-09-18 18:01:33 +0000

    criamos algortimo

---
+++ aula.js
@@ -0,0 +1,36 @@
+//Este código foi para treino de variáveis
+
+/*var rotulo = "natalya";
+var rotulo = 12345;
+console.log(rotulo);  
+*/
+
+//
+/*const numero1 = 10;
+const numero2 = 70;
+const soma = numero1 + numero2; 
+console.log(soma)
+
+const numero3 = 40
+const numero4 = 30
+const subtracao = numero3 - numero4
+console.log(subtracao)
+
+const multiplicacao = numero1 * numero2
+const divisao = numero3 / numero4
+console.log(`o resultado é: ${soma}`)
+console.log(`o resultado é: ${subtracao}`)
+console.log(`o resultado é: ${divisao}`)
+console.log(`o resultado é: ${multiplicacao}`)
+*/
+
+const nota1 = 8;
+const nota2 = 6;
+const nota3 = 10;
+const media = (nota1 + nota2 + nota3)/3
+console.log('O resultado da média é... ${media}')
+
+
+
+
+
