f5f2095ace0fb4438e37a0ca407f686e2d840113
pedrolucasavila <pedrocavalcanti772@gmail.com>
2024-10-01 15:35:33 -0300

    Update Atividade.js

---
+++ Atividade.js
@@ -0,0 +1,35 @@
+/*
+ Conversor de Moedas:
+ ● Crieumconversor de moedas, considerando taxas de câmbio atualizadas.
+ ● Asvariáveis devem guardar o valor das moedas inicial e final.
+ ● Exiba oresultado com uma mensagem personalizada
+ */
+
+let taxadedolar = 5.47
+let valor = 20000
+let valoremdolar = valor / taxadedolar
+console.log (`o dineiro convertido em dolar é: ${valoremdolar}`)  
+
+valor = 30000;
+
+/*  Convertendo Celsius para Fahrenheit
+ ● Crieumavariável para a temperatura em Celsius e faça a conversão para
+ Fahrenheit usando a fórmula (Celsius * 9/5) + 32.
+ ● Exiba oresultado com uma mensagem personalizada.
+ */
+
+let temperaturaCelsius = 40
+let temperaturaFahrenheit = (temperaturaCelsius * 9/5) + 32
+console.log (`O valor da temperatura em fahrn é: ${temperaturaFahrenheit}`);
+
+/*Calculadora de IMC (Índice de Massa Corporal)
+ ● Crievariáveis para peso (em kg) e altura (em metros).
+ ● Calcule o IMC utilizando a fórmula: IMC = peso / (altura * altura).
+ ● Exiba oresultado no console.
+ ● Pesquise sobre o método toFixed- Clique aqui
+ */
+
+let pesocorporal = 74kilos
+let altura = 1.74metros
+let Imc = pesocorporal / (altura * altura)
+console.log (`O valor do resultado é : ${Imc}`)
